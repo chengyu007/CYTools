07df582520ca3283efc9e9f3d38478dfa2fc84ea
chengyu007 <173859684@qq.com>
2017-12-04 10:37:46 +0800

    初始化

---
+++ Example/CYPtools/CYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CYAppDelegate.h
+//  CYPtools
+//
+//  Created by 173859684@qq.com on 12/04/2017.
+//  Copyright (c) 2017 173859684@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/Pods/Target Support Files/CYPtools/CYPtools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CYPtools : NSObject
+@end
+@implementation PodsDummy_CYPtools
+@end
